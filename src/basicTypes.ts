5309fc9ee3cdfdfd8dd4fd8034434d2124de1037
Ahmadreza Nouri   FrontEnd-ReactJs <104431754+ahmadrezanouri1@users.noreply.github.com>
2023-03-24 09:47:40 -0700

    fined basic types in typescript

---
+++ src/basicTypes.ts
@@ -0,0 +1,16 @@
+// types 
+
+let number1 : number;
+let string1 : string;
+let bool1 : boolean;
+let any1:any;
+let unknown1 : unknown;
+let null1:null;
+let object1:object={
+    
+}
+
+
+let array:Array<number> = [1]
+
+
